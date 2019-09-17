3b4de86db5cd0082a363b78e89f49c8e24c18f86
Adi Saputra <adi.saputra@binus.ac.id>
2019-09-17 22:57:58 +0700

    delete tanda tanya

--- application/views/master_product/tb_master_product_form.php
+++ application/views/master_product/tb_master_product_form.php
@@ -160,7 +160,7 @@
 
 
                             <div class='box-body'>
-                                <div class='form-group'>Description  #   ?>
+                                <div class='form-group'>Description  #   
                                     <input type="text" class="form-control" name="description" id="descriptions" placeholder=" " />
                                 </div>
                             </div>   
