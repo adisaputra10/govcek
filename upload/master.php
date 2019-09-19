50f7c7339c47db9f0decf4feedc7d5caf6a93734
Adi Saputra <adi.saputra@binus.ac.id>
2019-09-20 04:07:08 +0700

    update csv

--- upload/master.php
+++ upload/master.php
@@ -21,68 +21,84 @@ function getDataTable($table,$kolom,$val,$id) {
 
 }
 
+$csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
 
 
-////
-// upload file xls
-$target = basename($_FILES['filepegawai']['name']) ;
-move_uploaded_file($_FILES['filepegawai']['tmp_name'], $target);
+if(!empty($_FILES['filepegawai']['name']) && in_array($_FILES['filepegawai']['type'], $csvMimes)){
+        
+	// If the file is uploaded
+	if(is_uploaded_file($_FILES['filepegawai']['tmp_name'])){
+		
+		// Open uploaded CSV file with read-only mode
+		$csvFile = fopen($_FILES['filepegawai']['tmp_name'], 'r');
+		
+		// Skip the first line
+		fgetcsv($csvFile);
+		
+		// Parse data from CSV file line by line
+		while(($line = fgetcsv($csvFile)) !== FALSE){
+			// Get row data
+		 
 
-// beri permisi agar file xls dapat di baca
-chmod($_FILES['filepegawai']['name'],0777);
 
-// mengambil isi file xls
-$data = new Spreadsheet_Excel_Reader($_FILES['filepegawai']['name'],false);
-// menghitung jumlah baris data yang ada
-$jumlah_baris = $data->rowcount($sheet_index=0);
+			$category = $line[0];	
+			$product_category= $line[1];
+			$end_user	= $line[2];
+			$manufacture= $line[3];	
+			$serial	= $line[4];
+			$asset	= $line[5];
+			$service_date = $line[6];	
+			$expire_date = $line[7];	
+			$unit_system = $line[8];
+			$inspection_schedule = $line[9]	;
+			$capacity = $line[10];	
+			$quick_check = $line[11];	
+			$status = $line[12];
+			$description = $line[13];
+			
+			$id_category=getDataTable("category","category",$category,"id");
+			$id_product_category=getDataTable("product_category","product_category",$product_category,"id");
+			$id_end_user=getDataTable("end_user","company",$end_user,"id");
+			$id_manufacture=getDataTable("manufacture","manufacture_name",$manufacture,"id");
+			$id_unit_system=getDataTable("hp_unit","description",$unit_system,"id");
+			$id_status=getDataTable("status","status_name",$status,"id");
+			$id_inspection_schedule=getDataTable("inspection_schedule","inspection_name",$inspection_schedule,"id");
+			$uid="1";
 
-// jumlah default data yang berhasil di import
-$berhasil = 0;
-for ($i=2; $i<=$jumlah_baris; $i++){
 
-	// menangkap data dan memasukkan ke variabel sesuai dengan kolumnya masing-masing
- 
-	$category = $data->val($i, 1);	
-	$product_category= $data->val($i, 2);	
-	$end_user	= $data->val($i, 3);
-	$manufacture= $data->val($i, 4);	
-	$serial	= $data->val($i, 5);
-	$asset	= $data->val($i, 6);
-	$service_date = $data->val($i, 7);	
-	$expire_date = $data->val($i, 8);	
-	$unit_system = $data->val($i, 9);
-	$inspection_schedule = $data->val($i, 10);	
-	$capacity = $data->val($i, 11);	
-	$quick_check = $data->val($i, 12);	
-	$status = $data->val($i, 13);
-	$description = $data->val($i, 14);
-	
-	$id_category=getDataTable("category","category",$category,"id");
-	$id_product_category=getDataTable("product_category","product_category",$product_category,"id");
-	$id_end_user=getDataTable("end_user","company",$end_user,"id");
-	$id_manufacture=getDataTable("manufacture","manufacture_name",$manufacture,"id");
-	$id_unit_system=getDataTable("hp_unit","description",$unit_system,"id");
-	$id_status=getDataTable("status","status_name",$status,"id");
-	$id_inspection_schedule=getDataTable("inspection_schedule","inspection_name",$inspection_schedule,"id");
-	$uid="1";
-   /*
-	echo "<br>category $category <br>product_category $product_category <br>end_user $end_user<br> manufacture $manufacture <br>serial $serial<br>";
-	echo "asset $asset <br> service_date $service_date <br> expire_date $expire_date <br>expire_date $expire_date <br>unit_system $unit_system";
-	echo "<br>inspection_schedule $inspection_schedule<br> capacity $capacity <br>quick_check $quick_check <br>status $status";
-*/
-	if($category != "" && $product_category != "" && $end_user != ""){
-		// input data ke database (table data_pegawai)
-		$sql="INSERT INTO `mst_product` (`id`, `category_id`, `product_category_id`, `end_user_id`, `manufacture_id`, `serial_number`, `asset_number`, `service_date`, `expire_date`, `capacity`, `unit_system_id`, `inspection_schedule_id`, `quick_check_no`, `status`, `uid`,description) VALUES (NULL, '$id_category', '$id_product_category', '$id_end_user', '$id_manufacture', '$serial', '$asset', '$service_date', '$expire_date', '$capacity', '$id_unit_system', '$id_inspection_schedule', '$quick_check', '$id_status', '$uid','$description');";
-		echo "$sql";
-		mysqli_query($koneksi,$sql);
-		$berhasil++;
+
+
+			//echo "$name $email $phone $name1 $email1 $phone1 $name2 $email2 $phone2";
+
+
+
+			if($category != "" && $product_category != "" && $end_user != ""){
+				// input data ke database (table data_pegawai)
+				$sql="INSERT INTO `mst_product` (`id`, `category_id`, `product_category_id`, `end_user_id`, `manufacture_id`, `serial_number`, `asset_number`, `service_date`, `expire_date`, `capacity`, `unit_system_id`, `inspection_schedule_id`, `quick_check_no`, `status`, `uid`,description) VALUES (NULL, '$id_category', '$id_product_category', '$id_end_user', '$id_manufacture', '$serial', '$asset', '$service_date', '$expire_date', '$capacity', '$id_unit_system', '$id_inspection_schedule', '$quick_check', '$id_status', '$uid','$description');";
+				echo "$sql";
+				mysqli_query($koneksi,$sql);
+				//$berhasil++;
+			}
+
+
+
+		}
+
 	}
+
 }
 
-// hapus kembali file .xls yang di upload tadi
-unlink($_FILES['filepegawai']['name']);
 
-// alihkan halaman ke index.php
+
+
+
+
+
+
+
+
+
+
 
  
 ?>
